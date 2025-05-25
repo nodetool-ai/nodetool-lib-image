b2b258098d721d29bc42bf8bc0cc95f0662e829b
georgi <matti.georgi@gmail.com>
2025-05-25 13:43:17 +0200

    Add image processing nodes and DSL

---
+++ src/nodetool/nodes/nodetool/__init__.py
@@ -0,0 +1,2 @@
+from .image import *  # noqa: F401,F403
+from .output import *  # noqa: F401,F403
---
+++ src/nodetool/nodes/nodetool/image.py
@@ -0,0 +1,10 @@
+from ..lib.image import (
+    SaveImage,
+    GetMetadata,
+    BatchToList,
+    Paste,
+    Fit,
+    Scale,
+    Resize,
+    Crop,
+)
